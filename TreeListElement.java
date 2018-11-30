73414d851da65bd1689d957cdb32088d600ecdb3
WS2018progra <44625386+WS2018progra@users.noreply.github.com>
2018-11-30 08:49:18 +0100

    Update TreeListElement.java

--- TreeListElement.java
+++ TreeListElement.java
@@ -1,40 +1,44 @@
 public class TreeListElement{
-  private Tree value;
+    private Tree value;
     
-  private TreeListElement next;
+    private TreeListElement next;
 
-  /**
-  * @param inputValue The value of the new TreeListElement
-  * @param inputNext The next elements of the TreeListElement
-  */
-  public TreeListElement(Tree inputValue, TreeListElement inputNext){
-    this.value = inputValue;
-    this.next = inputNext;
-  }
-  
-  //a)
-  
-  public void setValue(Tree value){
-    this.value=value;
-  }
-
-  public Tree getValue(){
-    return this.value;
-  }
-
-  public TreeListElement getNext(){
-    return this.next;
-  }
-
-  public void setNext(TreeListElement next){
-    this.next=next;
-  }
-  //b)
-  
-  //c)
-  
-  //d)
-  
-  //e)
+    public TreeListElement(Tree inputValue, TreeListElement inputNext){
+        this.value = inputValue;
+        this.next = inputNext;
+    }
+    
+    public Tree getValue() {
+        return this.value;
+    }
+    //wird momentan, wie manche anderen Selektoren, garnicht benötigt - aus Routine erstellt.
+    public void setValue(Tree value) {
+        this.value = value;
+    }
+    
+    public TreeListElement getNext() {
+        return this.next;
+    }
+    
+    public void setNext(TreeListElement next) {
+        this.next = next;
+    }
+    
+    /**
+     * Berechnet die Stringrepräsentation des aktuellen TreeListElement-Objektes, sowie, rekursiv, aller per next-Referenz
+     * verbundenen Objekte der TreeList. Die einzelnen Repräsentationen der TreeListElemente werden mit Kommata von einander getrennt.
+     * Hierbei wird jeweils die Methode toString() der Klasse Tree aufgerufen um die Arbeit aufzuteilen.
+     * @return die String-Repräsentation des aktuellen, sowie, falls vorhanden, aller folgenden TreeListElement-Objekten.
+     */
+    public String toString() {
+        String s = "";
+        if(value!=null){
+            s += this.value.toString();
+        }
+        if(next!=null) {
+            s = s + "," + next.toString();
+        }
+        return s;
+    }
   
 }
